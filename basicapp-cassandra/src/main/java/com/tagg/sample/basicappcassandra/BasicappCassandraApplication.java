76dc5002ad3959024744be1cc6eb323fa40f009e
Sorasak Chotsiri <sorasak_cho@truecorp.co.th>
2018-06-15 16:19:20 +0700

    [NEW]Add project

---
+++ basicapp-cassandra/src/main/java/com/tagg/sample/basicappcassandra/BasicappCassandraApplication.java
@@ -0,0 +1,12 @@
+package com.tagg.sample.basicappcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicappCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicappCassandraApplication.class, args);
+	}
+}
